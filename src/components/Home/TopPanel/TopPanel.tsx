84fcdd79d4ef317440a212a93b8d98bc757efbff
Bruce Zhang <brucezhangpro@hotmail.com>
2021-01-25 13:39:52 -0500

    chore: Lint TopPanel />

--- src/components/Home/TopPanel/TopPanel.tsx
+++ src/components/Home/TopPanel/TopPanel.tsx
@@ -1,53 +1,53 @@
-import React from 'react';
-import {Component }from 'react';
-import {connect} from 'react-redux';
-import {Button, Collapse, CardBody, Card} from 'reactstrap';
-import OrganizationStateInterface from '../../../store/interface/OrganizationState.interface';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button, Collapse, CardBody, Card } from "reactstrap";
+import OrganizationStateInterface from "../../../store/interface/OrganizationState.interface";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './TopPanel.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./TopPanel.css";
 
 interface Props {
-    name: string,
-    description: string,
-    organizationName: string,
+  name: string;
+  description: string;
+  organizationName: string;
 }
 
 class TopPanel extends Component<Props> {
-    state = {
-        dropdownOpen: false,
-    };
-    render() {
-        return(
-            <>
-            <div className="TopPanelControl">
-                    <h2>{this.props.organizationName}</h2>
-                        <Button
-                            color="primary"
-                            onClick={() => { this.setState({ dropdownOpen: !this.state.dropdownOpen }) }}
-                            className="ButtonAbout">About</Button>
-            </div>
-            <Collapse className="Collspse" isOpen={this.state.dropdownOpen}>
-                <Card className="Card">
-                    <CardBody>
-                        {this.props.description}
-                    </CardBody>
-                </Card>
-            </Collapse>
-            </>
-        );
-    }
+  state = {
+    dropdownOpen: false,
+  };
+
+  render() {
+    return (
+      <>
+        <div className="TopPanelControl">
+          <h2>{this.props.organizationName}</h2>
+          <Button
+            color="primary"
+            onClick={() => {
+              this.setState({ dropdownOpen: !this.state.dropdownOpen });
+            }}
+            className="ButtonAbout"
+          >
+            About
+          </Button>
+        </div>
+        <Collapse className="Collspse" isOpen={this.state.dropdownOpen}>
+          <Card className="Card">
+            <CardBody>{this.props.description}</CardBody>
+          </Card>
+        </Collapse>
+      </>
+    );
+  }
 }
 
-
 const mapStateToProps = (state: OrganizationStateInterface) => {
-    return{
-        name: state.name,
-        organizationName: state.organizationName,
-        description: state.description,
-    };
+  return {
+    name: state.name,
+    organizationName: state.organizationName,
+    description: state.description,
+  };
 };
-
 
 export default connect(mapStateToProps)(TopPanel);

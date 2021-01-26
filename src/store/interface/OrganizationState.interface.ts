0a9462406707fd24c2f65d2949c2011c9e3d88a1
Bruce Zhang <brucezhangpro@hotmail.com>
2021-01-25 19:16:39 -0500

    chore: Linting Orgainization Interface

--- src/store/interface/OrganizationState.interface.ts
+++ src/store/interface/OrganizationState.interface.ts
@@ -1,11 +1,11 @@
-import Event from './Event.interface'
+import Event from './Event.interface';
 
 export default interface OrganizationStateInterface {
-    readonly name: string,
-    readonly description: string,
-    readonly organizationName: string,
-    readonly share: boolean,
-    readonly events: Event[],
-    readonly _id: string,
-    readonly email: string
+  readonly name: string;
+  readonly description: string;
+  readonly organizationName: string;
+  readonly share: boolean;
+  readonly events: Event[];
+  readonly _id: string;
+  readonly email: string;
 }

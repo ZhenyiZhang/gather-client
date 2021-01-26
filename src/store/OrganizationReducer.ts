f3093fe63999092a4ea7447f075e1cd155360830
Bruce Zhang <brucezhangpro@hotmail.com>
2021-01-26 16:35:59 -0500

    chore: Lint Organization reducer

--- src/store/OrganizationReducer.ts
+++ src/store/OrganizationReducer.ts
@@ -1,23 +1,23 @@
-import OrganizationStateInterface from '../store/interface/OrganizationState.interface';
+import OrganizationStateInterface from './interface/OrganizationState.interface';
 
 const initialState: OrganizationStateInterface = {
-    name : 'initial',
-    description: '',
-    organizationName: '',
-    events: [],
-    share: false,
-    email: '',
-    _id: ''
+  name: 'initial',
+  description: '',
+  organizationName: '',
+  events: [],
+  share: false,
+  email: '',
+  _id: '',
 };
 
 const OrganizationReducer = (state = initialState, action: any) => {
-    switch (action.type) {
-        case 'getProfile':
-            return {
-                ...action.organization
-            }
-    }
-    return state;
+  switch (action.type) {
+    case 'getProfile':
+      return {
+        ...action.organization,
+      };
+  }
+  return state;
 };
 
 export default OrganizationReducer;

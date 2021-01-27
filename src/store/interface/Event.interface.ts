a4eafe5e587f6e213f8bde428e9ecd89875b0c69
Bruce Zhang <brucezhangpro@hotmail.com>
2021-01-26 20:37:54 -0500

    chore: Lint Event interface

--- src/store/interface/Event.interface.ts
+++ src/store/interface/Event.interface.ts
@@ -1,18 +1,18 @@
-export default interface Event{
-    repeat: string,
-    name: string,
-    description: string,
-    Organization: string,
-    start: Date,
-    end: Date,
-    repeatEnds: Date,
-    repeatNeverEnds: boolean,
-    repeatExceptions? : Date[],
-     contacts: {
-        email: string,
-        link: string,
-        phone: string,
-        location: string
-    },
-    _id: string
-};
+export default interface Event {
+  repeat: string;
+  name: string;
+  description: string;
+  Organization: string;
+  start: Date;
+  end: Date;
+  repeatEnds: Date;
+  repeatNeverEnds: boolean;
+  repeatExceptions?: Date[];
+  contacts: {
+    email: string;
+    link: string;
+    phone: string;
+    location: string;
+  };
+  _id: string;
+}
